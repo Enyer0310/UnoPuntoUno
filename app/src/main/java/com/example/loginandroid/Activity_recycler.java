9c41aaf231f564e8db04ccff213a4b6f05382efe
Enyer0310 <enyer0010@gmail.com>
2022-11-30 01:27:02 +0100

    FirebaseListo

--- app/src/main/java/com/example/loginandroid/Activity_recycler.java
+++ app/src/main/java/com/example/loginandroid/Activity_recycler.java
@@ -1,15 +1,29 @@
 package com.example.loginandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.sql.DatabaseMetaData;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Activity_recycler extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    MyAdapter myAdapter;
+    ArrayList<User> list;
+    
 
     List<ListElment> elements;
 
@@ -17,6 +31,35 @@ public class Activity_recycler extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycle);
+
+        recyclerView = findViewById(R.id.listRecycle);
+        database = FirebaseDatabase.getInstance().getReference("Users");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        myAdapter = new MyAdapter(this,list);
+        recyclerView.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    User user = dataSnapshot.getValue(User.class);
+                    list.add(user);
+
+
+                }
+                myAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         init();
     }
---
+++ app/src/main/java/com/example/loginandroid/User.java
@@ -0,0 +1,13 @@
+package com.example.loginandroid;
+
+public class User {
+    String nameTextView, cityTextView;
+
+    public String getNameTextView() {
+        return nameTextView;
+    }
+
+    public String getCityTextView() {
+        return cityTextView;
+    }
+}
